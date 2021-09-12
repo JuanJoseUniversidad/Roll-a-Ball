4e4409f33e03989fc5e4cab7faee4cb63e9651d4
JuanJoseUniversidad <74938518+JuanJoseUniversidad@users.noreply.github.com>
2021-09-12 21:30:03 +0200

    PickUp tag Condition

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,6 +30,8 @@ public class PlayerController : MonoBehaviour
 	}
 	
 	private void OnTriggerEnter(Collider other){
-		other.gameObject.SetActive(false);
+		if(other.gameObject.CompareTag("PickUp")){
+			other.gameObject.SetActive(false);
+		}
 	}
 }
